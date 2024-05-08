5854c0a5a66327d6449ffe07c959971ba7d2adc3
akhilrockbabu.com <akhilrockbabu@gmail.com>
2024-05-08 15:01:06 +0530

    updated

---
+++ dbms/prgrm20.sql
@@ -0,0 +1,10 @@
+create or replace trigger t2 after insert or update on xy
+begin
+if inserting
+then insert into test values('inserted',CURRENT_TIMESTAMP);
+else
+insert into test values('updated',CURRENT_TIMESTAMP);
+end if;
+end;
+/
+
